4ca2ad0047748f1689cc5cce00bde9e06b6adb8c
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-05 20:40:28 +0300

    update

--- js/file.js
+++ js/file.js
@@ -1,6 +1,15 @@
-const start = 3;
-const end = 7;
-for (let i = 3; i <= 7; i += 1) {
-  console.log(i);
-}
+// const start = 3;
+// const end = 7;
+// for (let i = 3; i <= 7; i += 1) {
+//   console.log(i);
+// }
 
+function includes(array, value) {
+    // Change code below this line
+  for (i=0; i<array.length; i+=1) {
+    if (array[i] = value) {
+      return "true";
+    }
+  }
+    // Change code above this line
+  }
