041e40b292b9462b204f681d82f062018ff497d6
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-06 20:19:24 +0300

    iteration

---
+++ js/iteration.js
@@ -0,0 +1,11 @@
+const feedback = {
+    good :5,
+    neutral : 10,
+    bad : 3,
+};
+
+const keys = object.keys(feedback);
+const values = object.values(feedback);
+ 
+console.log(keys);
+console.log(values);
