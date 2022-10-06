2a97c7c731fadcae862fc72d6fd46b0d54cee1b5
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-06 19:38:07 +0300

    update

---
+++ js/playlist.js
@@ -0,0 +1,27 @@
+const playlist = {
+    name: 'My super playlist',
+    rating : 5,
+    tracks : ['track-1', 'track-2','track-3'],
+    trackCount : 3,
+};
+
+console.log (playlist.tracks);
+console.log (playlist.name);
+console.log (playlist['name']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fn = function (myObject) {
+//     console.log(myObject);
+// };
