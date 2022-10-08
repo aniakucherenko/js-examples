51eecab39eaeb63f23f31455fc69fae2b726ab2d
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-09 00:55:32 +0300

    update cart

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,46 @@
 const cart = {
     items: [],
     getItems() {
-        return
+        return this.items;  
     },
-    add () {},
-    remove () {},
-    clear () {},
-    countTotalPrice() {},
-    increaseQuantity () {},
-    decreaseQuantity () {},
+    add (product) {
+        const newProduct = {
+            ...product,
+            quantity: 0,
+        };
+
+        this.items.push(product);
+    },
+    remove (productName) {
+
+        for (let i = 0; i < this.items.length; i +=1) {
+
+            if (productName === this.items[i].name){
+                return 'The product is finded', productName;
+                this.items.splice(i, 1);
+            }
+        }
+    },
+    clear () {
+
+        this.items = [];
+    },
+    countTotalPrice() {
+        let total = 0;
+
+        for ( const item of this.items) {
+            total += item.price;
+        }
+        return total;
+    },  
+    increaseQuantity (productName) {},
+    decreaseQuantity (productName) {},
 
 };
+
+console
+
+cart.add({ name: 'apple', price: 50 });
+cart.add({ name: 'lemon', price: 60 });
+cart.add({ name: 'orange', price: 60 });
+cart.add({ name: 'strawberry', price: 110 });
