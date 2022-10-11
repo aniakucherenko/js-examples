02d75838428e16f83bc3929cc3c2fce1897e3040
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-11 16:58:25 +0300

    Update examples.js

--- js/examples.js
+++ js/examples.js
@@ -1,10 +1,21 @@
 
-function findMatches(numbers, ...args) {
-    const matches = []; 
-    for (const number of numbers) {
-      if (args.includes(number)) {
-        matches.push(number);
-      }
-    }
-    return matches;
+// function findMatches(numbers, ...args) {
+//     const matches = []; 
+//     for (const number of numbers) {
+//       if (args.includes(number)) {
+//         matches.push(number);
+//       }
+//     }
+//     return matches;
+//   }
+
+
+  function makePizza() {
+    return "Your pizza is being prepared, please wait.";
   }
+  // Change code below this line
+  
+  const result = null;
+  const pointer = null;
+  
+  
