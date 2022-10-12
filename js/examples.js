ecfaf5ae4dd023a3fb0139ef0f6442d36001a471
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-12 21:12:23 +0300

    callback

--- js/examples.js
+++ js/examples.js
@@ -33,25 +33,54 @@
 
   
 
-  function deliverPizza(pizzaName) {
-    return `Delivering ${pizzaName} pizza.`;
-  }
+  // function deliverPizza(pizzaName) {
+  //   return `Delivering ${pizzaName} pizza.`;
+  // }
   
-  function makePizza(pizzaName) {
-    return `Pizza ${pizzaName} is being prepared, please wait...`;
-  }
+  // function makePizza(pizzaName) {
+  //   return `Pizza ${pizzaName} is being prepared, please wait...`;
+  // }
   
-  function makeMessage(pizzaName, callback) {
-    return callback (pizzaName);
-  }
+  // function makeMessage(pizzaName, callback) {
+  //   return callback (pizzaName);
+  // }
 
 
-  
 
-  function repeatLog(n) {
-    for (let i = 0; i < n; i += 1) {
-      console.log(i);
+
+  // function repeatLog(n) {
+  //   for (let i = 0; i < n; i += 1) {
+  //     console.log(i);
+  //   }
+  // }
+  // repeatLog(5);
+
+
+  const pizzaPalace = {
+    pizzas: ["Ultracheese", "Smoked", "Four meats"],
+    order(pizzaName, onSuccess, onError) {
+    if (this.pizzas.includes(pizzaName)) {
+      return onSuccess(pizzaName);
     }
+      return onError (`There is no pizza with a name ${pizzaName} in the assortment.`);
+    },
+  };
+  
+  
+  // Callback for onSuccess
+  function makePizza(pizzaName) {
+    return `Your order is accepted. Cooking pizza ${pizzaName}.`;
   }
-  repeatLog(5);
+  
+  // Callback for onError
+  function onOrderError(error) {
+    return `Error! ${error}`;
+  }
+  
+  // Method calls with callbacks
+  pizzaPalace.order("Smoked", makePizza, onOrderError);
+  pizzaPalace.order("Four meats", makePizza, onOrderError);
+  pizzaPalace.order("Big Mike", makePizza, onOrderError);
+  pizzaPalace.order("Vienna", makePizza, onOrderError);
+  
   
