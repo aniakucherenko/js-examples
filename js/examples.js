39ca775725a49af4ba8dc1cfb8f751b5b50885d1
aniakucherenko <ania.kucherenko@gmail.com>
2022-10-13 18:23:41 +0300

    Update examples.js

--- js/examples.js
+++ js/examples.js
@@ -113,3 +113,28 @@
   //     });
   //   return newArray;
   // }
+
+
+
+
+  const books = [
+    {
+      title: "The Last Kingdom",
+      author: "Bernard Cornwell",
+      genres: ["adventure", "history"],
+    },
+    {
+      title: "Beside Still Waters",
+      author: "Robert Sheckley",
+      genres: ["fiction", "mysticism"],
+    },
+    {
+      title: "Redder Than Blood",
+      author: "Tanith Lee",
+      genres: ["horror", "mysticism", "adventure"],
+    },
+  ];
+  // Change code below this line
+  const allGenres = books.flatMap(book => book.genres);
+  const uniqueGenres = allGenres.filter((genre, index, array) => array.indexOf(genre) === index);
+  
